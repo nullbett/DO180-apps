124ed811ecd9c5d7ed3ea53a985f7b1820a8f731
Student User <student@workstation.lab.example.com>
2022-10-27 15:43:53 -0400

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,7 +2,7 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Some very cool change to substitute for the lack of ascii art! \n";
 print "___________________    . , ; . \n";
-print "(___________________|~~~~~X.;' . \n";
-print "                      ' `       \n";
+print "(___________________|~~~~~X.; . \n";
+print "                      ,  . , .    \n";
 print "              TNT               \n";
 ?>
